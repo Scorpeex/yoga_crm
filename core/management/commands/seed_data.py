9b1d7456dab26a0ee1f9bbebb34f64257f0a8b8c
Scorpeex <argentum.nrj@gmail.com>
2026-05-28 06:44:36 +0700

    start over

--- core/management/commands/seed_data.py
+++
@@ -1,9 +0,0 @@
-from django.core.management.base import BaseCommand
-from core.seed_data import run_seed
-
-
-class Command(BaseCommand):
-    help = 'Наполнение базы данных тестовыми данными'
-
-    def handle(self, *args, **options):
-        run_seed()
